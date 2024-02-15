504d637c45d42a16d9e249fd50d8e6a05d376e32
MrazTevin <millatevin@gmail.com>
2024-02-15 23:12:13 +0300

    m: setup the project

---
+++ src/test/java/com/ke/institutions/InstitutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ke.institutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

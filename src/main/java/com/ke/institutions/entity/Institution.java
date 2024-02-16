a5d9870abe1292447e782819fdee468f31337335
MrazTevin <millatevin@gmail.com>
2024-02-16 08:16:24 +0300

    m: provide getters and setters

--- src/main/java/com/ke/institutions/entity/Institution.java
+++ src/main/java/com/ke/institutions/entity/Institution.java
@@ -1,8 +1,13 @@
 package com.ke.institutions.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
+@Setter
+@Getter
 @Entity
 public class Institution {
     @Id
--- src/main/java/com/ke/institutions/entity/Student.java
+++ src/main/java/com/ke/institutions/entity/Student.java
@@ -1,9 +1,13 @@
 package com.ke.institutions.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.print.attribute.standard.MediaSize;
 
+@Setter
+@Getter
 @Entity
 public class Student {
     @Id
@@ -11,9 +15,8 @@ public class Student {
     private Long id;
 
     private String name;
+
     @ManyToOne
     @JoinColumn(name = "course_id")
     private Course course;
-
-
 }

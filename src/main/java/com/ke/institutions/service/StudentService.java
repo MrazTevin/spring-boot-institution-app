6187ed2616cf69b15d27bbbe64ecb9cc076841f0
MrazTevin <millatevin@gmail.com>
2024-02-16 09:55:24 +0300

    m: implement student service

--- src/main/java/com/ke/institutions/service/StudentService.java
+++ src/main/java/com/ke/institutions/service/StudentService.java
@@ -1,2 +1,40 @@
-package com.ke.institutions.service;public class StudentService {
+package com.ke.institutions.service;
+
+import com.ke.institutions.entity.Student;
+import com.ke.institutions.respository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    // CRUD operations for the Students
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student getStudent(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
 }

36e186b51b4c027f5432d99e69b690c2259b87a0
MrazTevin <millatevin@gmail.com>
2024-02-16 09:32:35 +0300

    m: implement the course service

---
+++ src/main/java/com/ke/institutions/service/CourseService.java
@@ -0,0 +1,37 @@
+package com.ke.institutions.service;
+
+import com.ke.institutions.entity.Course;
+import com.ke.institutions.respository.CourseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+    private final CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    // CRUD operations for courses
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public Course getCourse(Long id) {
+        return courseRepository.findById(id).orElse(null);
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Course updateCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+ }

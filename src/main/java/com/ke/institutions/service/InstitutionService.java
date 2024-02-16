16f929aa217c68ea10d1aa7b069ed8f2b6d14338
MrazTevin <millatevin@gmail.com>
2024-02-16 09:26:23 +0300

    m: implement institution service

---
+++ src/main/java/com/ke/institutions/service/InstitutionService.java
@@ -0,0 +1,39 @@
+package com.ke.institutions.service;
+
+import com.ke.institutions.entity.Institution;
+import com.ke.institutions.respository.InstitutionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InstitutionService {
+    private final InstitutionRepository institutionRepository;
+    @Autowired
+    public InstitutionService(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
+    // CRUD DB operations
+
+    public Institution createInstitution(Institution institution) {
+        return institutionRepository.save(institution);
+    }
+
+    public Institution getInstitution(Long id) {
+        return institutionRepository.findById(id).orElse(null);
+    }
+
+    public List<Institution> getAllInstitutions() {
+        return institutionRepository.findAll();
+    }
+
+    public Institution updateInstitution(Institution institution) {
+        return institutionRepository.save(institution);
+    }
+
+    public void deleteInstitution(Long id) {
+        institutionRepository.deleteById(id);
+    }
+}

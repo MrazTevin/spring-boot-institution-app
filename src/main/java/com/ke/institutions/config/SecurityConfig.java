d41320e8f49f37dfdd298ba80cd2160529b0cd5b
MrazTevin <millatevin@gmail.com>
2024-02-25 03:36:53 +0300

    m: modify security configs

---
+++ src/main/java/com/ke/institutions/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.ke.institutions.config;
+
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${USER_PASSWORD")
+    private String userPassword;
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("admin")
+                .password(passwordEncoder().encode(userPassword))
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
+        return userDetailsService;
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception {
+             http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(new AntPathRequestMatcher("/api/**:w")).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin.permitAll())
+                .logout(logout -> logout.permitAll());
+        return http.build();
+    }
+}

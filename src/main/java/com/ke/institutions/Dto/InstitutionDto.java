734f68224c30c63acff5cfb4473bcf91077548e7
MrazTevin <millatevin@gmail.com>
2024-02-26 21:08:08 +0300

    m: create endpoint responses

--- src/main/java/com/ke/institutions/Dto/InstitutionDto.java
+++ src/main/java/com/ke/institutions/Dto/InstitutionDto.java
@@ -1,2 +1,13 @@
-package com.ke.institutions.Dto;public class InstitutionDto {
+package com.ke.institutions.Dto;
+
+import com.ke.institutions.entity.Institution;
+import lombok.Data;
+
+@Data
+public class InstitutionDto {
+
+    private String status;
+    private int statusCode;
+    private String message;
+    private Institution institution;
 }

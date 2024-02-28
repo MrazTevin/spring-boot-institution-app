f4ea9334695acde3d3c9bdab8fcf3f7599f570a2
MrazTevin <millatevin@gmail.com>
2024-02-28 22:08:15 +0300

    m: implement new object layer

--- src/main/java/com/ke/institutions/Dto/InstitutionDto.java
+++ src/main/java/com/ke/institutions/Dto/InstitutionDto.java
@@ -6,8 +6,7 @@ import lombok.Data;
 @Data
 public class InstitutionDto {
 
-    private String status;
-    private int statusCode;
-    private String message;
-    private Institution institution;
+    private Long institutionId;
+    private String name;
+
 }
